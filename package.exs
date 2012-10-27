6844269e093a83f1a5ab066b61085b2a0d9c286d
Marcelo Gornstein <marcelog@gmail.com>
2012-10-27 18:00:26 -0300

    added package.exs

---
+++ package.exs
@@ -0,0 +1,8 @@
+Expm.Package.new(
+  name: "jsonex",
+  description: "Wrapper for json",
+  version: "0.1",
+  keywords: ["json"],
+  maintainers: [[name: "Marcelo Gornstein", email: "marcelog@gmail.com"]],
+  repositories: [[github: "marcelog/jsonex", tag: "0.1"]]
+)
